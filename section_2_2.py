0c6bc12aa00e35ecf8111d23c9206c4052135764
azzadri007 <azzadri007@gmail.com>
2020-11-14 16:21:39 +0000

    Re-done 2.1 and 2.2

--- section_2_2.py
+++ section_2_2.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-N_total = 100 # # of training points (training data)
+N_total = 1000 # # of training points (training data)
 train_split_ratio = 0.75# training split as a ratio of total data:  # of training / total # of data
 N_train = int(0.75*N_total) # this will floor if not whole number
 N_test = N_total - N_train # associate remaining total to testing
 
 influence = 'random'
 # random influence - separating after sorting
-if influence == "random":
+if influence == "random": 
     rn = np.random.uniform(0, 1, N_total)
     x = list(np.sort(rn, axis=0))
     rand_positions = random.sample(range(N_total), N_test) # random array of N_test(-number) positions to give random points of data associated with testing data
@@ -21,6 +21,7 @@ if influence == "random":
 
     x_train = np.reshape(x, (N_train, 1))
     x_test = np.reshape(x_test_list, (N_test, 1))
+    print("Random Test-Train split")
 
 # non-random shuffle - separate before sorting
 else:
@@ -28,12 +29,13 @@ else:
     rn_test = np.random.uniform(0, 1, N_test)
     x_train = np.reshape(np.sort(rn_train, axis=0), (N_train, 1))
     x_test =  np.reshape(np.sort(rn_test, axis=0), (N_test, 1))
-
+    print("Seperate Random Generation")
 ####### Generate Training Data ###########################################################################################
 degree_p = N_test
 penalty = 0.0000001
 degree_list = [i for i in range(1, degree_p)]
-mu_list = [x_train[i] for i in range(1, len(x_train))]
+list_ = np.sort( np.random.randint(np.size(x_train, axis=0), size=degree_p-1) )
+mu_list = [[float(x_train[list_[i]]) for i in range(degree_p-1)] for j in range(N_train)]
 
 noise = np.random.normal(0, 0.01, N_train) # determine noise for given equation in section 2
 def get_y(x, noise):
@@ -54,19 +56,14 @@ def make_design(x, basisfn, basisfn_locs=None): # basisfn_locs is acts as other 
 # Determining Indivindual Design Matrices
 design_A = make_design(x_train, polynomial_basis_fn, degree_list)
 design_B = make_design(x_train, gaussian_basis_fn, mu_list)
-print(len(mu_list))
 
 # Generate Weights using alternative matrix equation given in section 2.2, page 2, footnote 2 as is independant of degree_p 
 def weight_gen(matrix, N, penalty, y):
     return  (matrix.T).dot( np.linalg.inv(( matrix.dot(matrix.T) + (penalty*np.identity(N)) )) ).dot(y)
 w_A = weight_gen(design_A, N_train, penalty, true_y)
 w_B = weight_gen(design_B, N_train, penalty, true_y)
-print(design_A, "\n\n ", len(design_B))
 
 ####### Generate Test Data ###########################################################################################
-mu_list = [x_train[i] for i in range(1, len(x_train))] 
-degree_list = [i for i in range(1, degree_p)]
-
 design_A_test = make_design(x_test, polynomial_basis_fn, degree_list)
 design_B_test = make_design(x_test, gaussian_basis_fn, mu_list)
 
